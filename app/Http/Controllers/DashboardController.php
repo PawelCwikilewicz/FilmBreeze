20beac6cb6f5b964023fedef026d72e384759e94
Pablo112a <pcwikilewicz@gmail.com>
2024-11-15 18:54:52 +0100

    quickfix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,31 +10,6 @@ class DashboardController extends Controller
 {
     public function index(){
         
-        // $host = "filmbreeze.clq0oucua0k7.eu-north-1.rds.amazonaws.com"; // np. adres IP lub nazwa domeny serwera bazy danych
-        // $dbname = "filmbreezeDB";
-        // $username = "admin";
-        // $password = "admin123";
-        
-        // try {
-        //     // Tworzenie połączenia za pomocą PDO
-        //     $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
-        //     $pdo = new PDO($dsn, $username, $password);
-            
-        //     // Ustawienia PDO dla błędów i wyjątków
-        //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-        //     echo "Połączono pomyślnie!";
-            
-        //     // Tutaj możesz wykonywać zapytania do bazy danych
-        // }
-        // catch (PDOException $e) {
-        //     // Obsługa błędu połączenia
-        //     echo "Połączenie nieudane: " . $e->getMessage();
-        // }
-        
-        // // Zamknięcie połączenia przez ustawienie obiektu na null (opcjonalne)
-        // $pdo = null;
-        
 
         $movies = [
             [
