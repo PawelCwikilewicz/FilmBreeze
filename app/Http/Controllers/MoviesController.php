53157e788d6c74d4b25463399ff43ca0583948db
Sebastian Prawucki <sebastian.prawucki@gmail.com>
2024-12-05 00:05:30 +0100

    Filmy i Seriale- display i baza danych

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
+use App\Models\Serie;
 use App\Models\User;
 
 class MoviesController extends Controller
 {
     public function index(){
         $movies = Movie::all();
-        return view('movies', compact('movies'));
+        $series = Serie::all();
+        return view('movies', compact('movies', 'series'));
 
     }
 
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -5,26 +5,24 @@
 @auth
     <x-slot:movies>
         @include('components.movieInfoForm')
+        <!-- FILMY -->
         <h2 class="movie-header"> Filmy </h2>
         <div class="movie-list">
-
-        
-
-        @foreach($movies as $movie) 
+        @foreach($movies as $movie)
             <li id="movie-list" class="movie-item" data-title="{{ $movie->title }}" data-movie-id="{{ $movie->id }}" data-description="{{ $movie->description }}">
-
                 <img src="{{ $movie->imagePath }}" alt="{{ $movie->title }}">
             </li>
         @endforeach
     </div>
+    <!-- SERIALE -->
     <h2 class="movie-header"> Seriale </h2>
     <div class="movie-list">
-        @foreach($movies as $movie)
-            <li id="movie-list">
-                <img src="{{ $movie->imagePath }}" alt="{{ $movie->title }}">
+        @foreach($series as $serie)
+            <li id="movie-list" class="movie-item" data-title="{{ $serie->title }}" data-movie-id="{{ $serie->id }}" data-description="{{ $serie->description }}">
+                <img src="{{ $serie->imagePath }}" alt="{{ $serie->title }}">
             </li>
         @endforeach
-</div>
+    </div>
     </x-slot:movies>
     @endauth
     @guest
