815b49e63c94d76c71e02a609c795323d4469f62
Pablo112a <pcwikilewicz@gmail.com>
2024-12-07 23:49:47 +0100

    watchlist slider

--- resources/views/watchlist.blade.php
+++ resources/views/watchlist.blade.php
@@ -5,12 +5,31 @@
 
     @auth
     <x-slot:container1>
+       
+
         <div class="container">
             @if($watchlistItems->isEmpty())
                 <p>Twoja lista jest pusta.</p>
             @else
+            <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
                 @foreach ($watchlistItems as $item)
-                    <div class="watchlist-item" data-movie-id="{{ $item->movie->id }}">
+                      <div class="swiper-slide" data-movie-id="{{ $item->movie->id }}">@if($item->movie->imagePath)
+                        <img src="{{ asset($item->movie->imagePath) }}" alt="{{ $item->movie->title }}">
+                    @else<p>No image available</p>
+                    @endif<!-- Remove -->
+                    <form action="{{ route('watchlist.remove') }}" method="POST" class="remove-form">
+                        <p class="font-bold">{{$item->movie->title}}</p>
+                        <br>
+                        <p id="movie-description" class="text-justify ml-1 mr-1 text-base">{{$item->movie->description}}</p>
+                        <br>
+                        @csrf
+                        <input type="hidden" name="movieId" value="{{ $item->movie->id }}">
+                        <button type="submit" class="remove-button classic-button" id="close-form-button-forum">Remove from Watchlist</button>
+                    </form></div>
+                
+                    
+                    {{-- <div class="watchlist-item" data-movie-id="{{ $item->movie->id }}">
                         @if($item->movie->imagePath)
                             <img src="{{ asset($item->movie->imagePath) }}" alt="{{ $item->movie->title }}">
                         @else
@@ -23,10 +42,21 @@
                             <input type="hidden" name="movieId" value="{{ $item->movie->id }}">
                             <button type="submit" class="remove-button">Remove from Watchlist</button>
                         </form>
-                    </div>
+                    </div> --}}
                 @endforeach
+            </div>
+            <div class="swiper-pagination"></div>
+          </div>
             @endif
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+        <script>
+            var swiper = new Swiper(".mySwiper", {
+              pagination: {
+                el: ".swiper-pagination",
+              },
+            });
+          </script>
     </x-slot:container1>
     @endauth
 
