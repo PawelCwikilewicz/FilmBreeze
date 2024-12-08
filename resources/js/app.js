a974e0a3a965cb4d27b048f4668d4a8ef8740fc8
Pablo112a <pcwikilewicz@gmail.com>
2024-12-08 12:45:36 +0100

    forum usuwanie, poprawki widokow mobilnych

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
             const icons = menuButton.querySelectorAll("svg[data-slot='icon']");
             icons.forEach(icon => icon.classList.toggle("hidden"));
+            icons.forEach(icon => icon.classList.toggle("block"));
         });
     } else {
         console.error("Menu button or mobile menu not found!");
@@ -169,4 +170,5 @@ document.getElementById('movieForm').addEventListener('submit', function(event) 
 
     // Wyczyść formularz
     document.getElementById('movieForm').reset();
+    location.reload();
 });
